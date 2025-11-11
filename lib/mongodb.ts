d4d0c3e907a8c91b97b73a54bf2eabc775c26404
Misbah Aiman <misbahaiman652gmail.com>
2025-11-11 19:20:02 +0500

    updated

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,19 +1,17 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, MongoClientOptions } from 'mongodb';
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
 }
 
 const uri = process.env.MONGODB_URI;
-const options = {};
+const options: MongoClientOptions = {};
 
-let client;
+let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === 'development') {
-  let globalWithMongo = global as typeof globalThis & {
-    _mongoClientPromise?: Promise<MongoClient>;
-  };
+  let globalWithMongo = global as typeof globalThis & { _mongoClientPromise?: Promise<MongoClient> };
 
   if (!globalWithMongo._mongoClientPromise) {
     client = new MongoClient(uri, options);
